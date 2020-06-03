00717c8a7ad8ff3bb2b618a1956aa878e70b8432
GabeAlexander <43545418+GabeAlexander@users.noreply.github.com>
2020-06-03 16:09:46 -0400

    change streams

--- main.js
+++ main.js
@@ -1,30 +1,44 @@
-const { app, BrowserWindow } = require('electron');
+const {
+    app,
+    BrowserWindow
+} = require('electron');
 const pie = require('puppeteer-in-electron');
 const puppeteer = require('puppeteer-core');
 
-let autoCollection;
-let page;
+let streamPages;
 const main = async () => {
     await pie.initialize(app);
     const browser = await pie.connect(app, puppeteer);
 
     const window = new BrowserWindow({
-       width: 1920,
-       height: 1080,
-       show: false
+        width: 1920,
+        height: 1080,
+        show: false
     });
-    const url = "https://www.twitch.tv/lana_lux";
+    const window2 = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        show: false
+    });
+    const url = "https://www.twitch.tv/floodz";
+    const url2 = "https://www.twitch.tv/lana_lux";
     await window.loadURL(url);
-    page = await pie.getPage(browser, window);
-    autoCollection = setInterval(collectPoints, 10000);
+    await window2.loadURL(url2);
+    let page = await pie.getPage(browser, window);
+    let page2 = await pie.getPage(browser, window2);
+    streamPages = await browser.pages();
+    setInterval(collectPoints, 10000);
 }
 
 async function collectPoints() {
-    let claimPoints = await page.$('.claimable-bonus__icon');
-    if(claimPoints) {
-        setTimeout(() => {
-            claimPoints.click();
-        }, 5000)
+    let pageList = streamPages;
+    for (streamPage of pageList) {
+        let claimPoints = await streamPage.$('.claimable-bonus__icon');
+        if (claimPoints) {
+            setTimeout(() => {
+                claimPoints.click();
+            }, 5000)
+        }
     }
 }
 
