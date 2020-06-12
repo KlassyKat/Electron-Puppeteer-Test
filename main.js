67939ea32b5795c2e90818710a4555f5c7423910
gabealexander <gabealexander01@gmail.com>
2020-06-11 22:28:24 -0400

    Added some QOL info to console logs

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//When using this for the first time you will need to
+//change the show: false to show: true and login into twitch in the window.
+//you can then change it back and run the script invisibly.
 const {
     app,
     BrowserWindow
@@ -5,10 +8,11 @@ const {
 const pie = require('puppeteer-in-electron');
 const puppeteer = require('puppeteer-core');
 
-let streamer = ''
+let streamer = 'radpuppy'
 
 let page;
 const main = async () => {
+    console.log('Starting auto-collection on ' + streamer + ' stream.');
     await pie.initialize(app);
     const browser = await pie.connect(app, puppeteer);
 
@@ -28,7 +32,7 @@ let collectionCount = 1;
 async function collectPoints() {
     let claimPoints = await page.$('.claimable-bonus__icon');
     if (claimPoints) {
-        console.log('Collected ' + streamer + ' ' + collectionCount);
+        console.log('Collected ' + streamer + ' ' + collectionCount + '    ' + createTimeStamp());
         collectionCount += 1;
         setTimeout(() => {
             claimPoints.click();
@@ -37,3 +41,11 @@ async function collectPoints() {
 }
 
 main();
+
+function createTimeStamp() {
+    let d = new Date();
+    let h = Number(d.getHours()) <= 12 ? d.getHours() : (d.getHours() - 12);
+    let m = d.getMinutes().toString().length == 1 ? '0' + d.getMinutes() : d.getMinutes();
+    let timeStamp = h + ':' + m;
+    return timeStamp;
+}
